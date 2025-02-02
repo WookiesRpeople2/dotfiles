baa942fb3a6c7a5e4a25f5bdf17ba561e5f53559
WookiesRpeople2 <keenantobiansky@gmail.com>
2025-02-03 00:03:46 +0100

    Changed over to lazyVim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,47 +1,8 @@
--- lua/config/autocmds.lua
-
-local augroup = vim.api.nvim_create_augroup
-
-local autocmd = vim.api.nvim_create_autocmd
-
-
-
--- Highlight on yank
-
-augroup('YankHighlight', { clear = true })
-
-autocmd('TextYankPost', {
-
-  group = 'YankHighlight',
-
-  callback = function()
-
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '1000' })
-
-  end
-
-})
-
-
-
--- Remove whitespace on save
-
-autocmd('BufWritePre', {
-
-  pattern = '',
-
-  command = ":%s/\\s\\+$//e"
-
-})
-
-
-
--- Don't auto comment new lines
-
-autocmd('BufEnter', {
-
-  pattern = '',
-
-  command = 'set fo-=c fo-=r fo-=o'
-
-})
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,75 +1,9 @@
 return {
-
-  'nvim-treesitter/nvim-treesitter',
-
-  build = ':TSUpdate',
-
-  config = function()
-
-    require('nvim-treesitter.configs').setup({
-
-      ensure_installed = {
-
-        "c",
-
-        "cpp",
-
-        "c_sharp",
-
-        "typescript",
-
-        "tsx",
-
-        "lua",
-
-        "vim",
-
-        "vimdoc",
-
-        "query",
-
-        "make"
-
-      },
-
-      sync_install = false,
-
-      auto_install = true,
-
-      highlight = {
-
-        enable = true,
-
-        additional_vim_regex_highlighting = false,
-
-      },
-
-      indent = {
-
-        enable = true
-
-      },
-
-      incremental_selection = {
-
-        enable = true,
-
-        keymaps = {
-
-          init_selection = "<CR>",
-
-          node_incremental = "<CR>",
-
-          node_decremental = "<BS>",
-
-          scope_incremental = "<TAB>",
-
-        },
-
-      },
-
-    })
-
-  end
-
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "lua", "javascript", "typescript", "python", "c" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
 }
